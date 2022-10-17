af6440aec63fb01a753e8a4e102d35163bbaf315
Roberto Pacheco <robertopvc@gmail.com>
2022-10-17 14:04:55 -0300

    add config file

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+DIR_NAME = os.path.dirname(__file__)
+
+dataset_name = "caltech256"
+model_name = "mobilenet"
+split_ratio = 0.1
+batch_size_train = 256
+h_flip_prob = 0.5
+equalize_prob = 0.5
+input_dim = 256
+dim = 224
+seed = 42
