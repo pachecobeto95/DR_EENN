032837506f92afc7f32dc111acceb78dc9d1eb35
Roberto Pacheco <robertopvc@gmail.com>
2022-12-07 23:38:05 -0300

    experimenting ensemble

--- ensemble_eednn_robust_analysis.py
+++ ensemble_eednn_robust_analysis.py
@@ -133,7 +133,7 @@ def exp_ensemble_analysis(args, df_backbone, df_ee, save_path, distortion_type):
 			acc_edge_dict = extract_accuracy_edge(df_backbone, df_ee, n_branch_edge, n_exits, threshold, distortion_levels, 
 				distortion_type)
 
-			save_results(acc_edge_dict, edge_prob_dict, distortion_levels, n_branch, threshold, save_path)
+			save_results(acc_edge_dict, edge_prob_dict, distortion_levels, n_branch_edge, threshold, save_path)
 
 
 def save_results(acc_edge_dict, edge_prob_dict, distortion_levels, n_branch, threshold, save_path):
