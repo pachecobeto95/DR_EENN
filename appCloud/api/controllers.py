2f9879641205c59700c0f67b5d3745fd1014ca42
Roberto Pacheco <robertopvc@gmail.com>
2022-12-23 13:18:42 -0300

    inference time xp

---
+++ appCloud/api/controllers.py
@@ -0,0 +1,96 @@
+from flask import Blueprint, g, render_template, request, jsonify, session, redirect, url_for, current_app as app
+import json, os, time, sys, config
+from .services import cloudProcessing
+
+api = Blueprint("api", __name__, url_prefix="/api")
+
+
+# Define url for the user send the image
+@api.route('/cloud/cloud_ee_inference', methods=["POST"])
+def cloud_ee_inference():
+	"""
+	This function receives an image or feature map from edge device (Access Point)
+	"""
+
+	data_edge = request.json
+
+	result = cloudProcessing.eeDnnInference(data_edge)
+
+	if (result["status"] ==  "ok"):
+		return jsonify(result), 200
+
+	else:
+		return jsonify(result), 500
+
+
+
+@api.route('/cloud/cloud_ensemble_inference', methods=["POST"])
+def cloud_ensemble_inferece():
+	"""
+	This function receives an image from user or client with smartphone or even a insurance camera 
+	into smart sity context
+	"""
+
+	data_edge = request.json
+
+	result = cloudProcessing.ensembleDnnInference(data_edge)
+
+	if (result["status"] ==  "ok"):
+		return jsonify(result), 200
+
+	else:
+		return jsonify(result), 500
+
+
+
+@api.route('/cloud/cloud_naive_ensemble_inference', methods=["POST"])
+def cloud_naive_ensemble_inferece():
+	"""
+	This function receives an image from user or client with smartphone or even a insurance camera 
+	into smart sity context
+	"""
+
+	data_edge = request.json
+
+	result = cloudProcessing.naiveEnsembleDnnInference(data_edge)
+
+	if (result["status"] ==  "ok"):
+		return jsonify(result), 200
+
+	else:
+		return jsonify(result), 500
+
+
+
+@api.route('/cloud/cloud_backbone_inference', methods=["POST"])
+def cloud_backbone_inference():
+	"""
+	This function receives an image from user or client with smartphone or even a insurance camera 
+	into smart sity context
+	"""
+
+	fileImg = request.files['img']
+	params = json.load(request.files['data'])
+
+	result = cloudProcessing.backboneDnnInference(fileImg, params)
+
+	if (result["status"] ==  "ok"):
+		return jsonify(result), 200
+
+	else:
+		return jsonify(result), 500
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ appCloud/api/services/cloudProcessing.py
@@ -0,0 +1,91 @@
+from flask import jsonify, session, current_app as app
+import os, pickle, requests, sys, config, time, torch, cv2, datetime, time, io, utils, json
+import numpy as np
+import torchvision.models as models
+import pandas as pd
+import torchvision.transforms as transforms
+from PIL import Image
+
+ee_model = utils.init_ee_dnn(device)
+
+backbone_model = utils.init_backbone_dnn(device)
+
+
+def eeDnnInference(data_edge):
+	#try:
+
+	tensor, conf_list, p_tar, n_branch_edge = torch.Tensor(data_edge["feature"]), data_edge["conf"], data_edge["p_tar"], data_edge["n_branch_edge"] 
+
+	output, conf_list, inf_class = eeDnnInferenceCloud(tensor, conf_list, p_tar, n_branch_edge)
+
+	return {"status": "ok"}
+
+
+def ensembleDnnInference(data_edge):
+
+
+	tensor, conf_list, p_tar, n_branch_edge = torch.Tensor(data_edge["feature"]), data_edge["conf"], data_edge["p_tar"], data_edge["n_branch_edge"] 
+
+	ee_model.eval()
+
+	with torch.no_grad():
+		conf_list, infer_class = ee_model.forwardEnsembleInferenceCloud(tensor.float(), conf_list, p_tar, n_branch_edge)
+
+	return {"status": "ok"}
+
+
+def naiveEnsembleDnnInference(data_edge):
+
+
+	tensor, conf_list, p_tar, n_branch_edge = torch.Tensor(data_edge["feature"]), data_edge["conf"], data_edge["p_tar"], data_edge["n_branch_edge"] 
+
+	ee_model.eval()
+
+	with torch.no_grad():
+		conf_list, infer_class = ee_model.forwardNaiveEnsembleInferenceCloud(tensor.float(), conf_list, p_tar, n_branch_edge)
+
+	return {"status": "ok"}
+
+
+def backboneDnnInference(fileImg, params):
+
+	image_bytes = fileImg.read()
+
+	img_tensor = utils.transform_image(image_bytes).to(device)
+
+	starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+
+	starter.record()
+
+	output = backbone_model(data)
+	conf, inf_class = torch.max(softmax(output), 1)
+
+	ender.record()
+	torch.cuda.synchronize()
+	inf_time = starter.elapsed_time(ender)
+
+	saveInferenceTime(inf_time, params, device)
+
+	return {"status": "ok"}
+
+
+
+def saveInferenceTime(inf_time, params, device):
+
+	result = {"inference_time": inference_time}
+	result.update(params)
+
+	df = pd.DataFrame(result)
+	df.to_csv(config.save_backbone_inf_time_path, mode='a', header=not os.path.exists(config.save_backbone_inf_time_path) )
+
+
+def eeDnnInferenceCloud(tensor, conf_list, p_tar, n_branch_edge):
+
+	ee_model.eval()
+
+	with torch.no_grad():
+		conf_list, infer_class = ee_model.forwardCoEeInferenceCloud(tensor.float(), conf_list, p_tar, n_branch_edge)
+
+	return output, conf_list, infer_class
+
+
