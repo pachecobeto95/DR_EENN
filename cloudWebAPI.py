20951201b41cc0390537cac5c3ae9904b0fe77b4
Roberto Pacheco <robertopvc@gmail.com>
2022-12-24 14:54:49 -0300

    run inference time exp

--- cloudWebAPI.py
+++ cloudWebAPI.py
@@ -7,4 +7,4 @@ iNITIALIZE Cloud API
 
 # configuring Host and Port from configuration files. 
 app.debug = False
-app.run(host="0.0.0.0" port=config.PORT_CLOUD)
+app.run(host="0.0.0.0", port=config.PORT_CLOUD)
