75af8324dd6523adfaed92be8e8b98fa8e15ab15
pachecobeto95 <robertopvc@gmail.com>
2023-09-13 14:03:26 -0300

    a

--- compute_missed_deadline_eednn_based.py
+++ compute_missed_deadline_eednn_based.py
@@ -220,7 +220,7 @@ def main(args):
 	df_pristine = df_inf_data[df_inf_data.distortion_type_data == "pristine"]
 	df_blur = df_inf_data[df_inf_data.distortion_type_data == "gaussian_blur"]
 
-	print(df_inf_time[(df_inf_time.distortion_type_data == "pristine") & (df_inf_time.threshold == 0.8)])
+	print(df_inf_time[(df_inf_time.distortion_type_data == "pristine") & (df_inf_time.threshold == df_inf_time.threshold.unique()[2])])
 	print(df_inf_time.threshold.unique())
 	sys.exit()
 
