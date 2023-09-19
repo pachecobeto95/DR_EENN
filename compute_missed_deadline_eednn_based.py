12ff9f95aff6c1740476d6c1233051c99c8cf3e2
pachecobeto95 <robertopvc@gmail.com>
2023-09-19 10:16:19 -0300

    a

--- compute_missed_deadline_eednn_based.py
+++ compute_missed_deadline_eednn_based.py
@@ -69,7 +69,7 @@ def computeMissedDeadlineProb(df_batches, df_inf_time, threshold, t_tar, n_branc
 		overall_acc = computeOverallAccuracy(df_batch, threshold, n_branches, inf_mode)
 		inference_time = computeInferenceTime(df_inf_time_batch, threshold, n_branches, inf_mode)
 
-		print(overall_acc, inference_time)
+		#print(overall_acc, inference_time)
 		missed_deadline += 1 if((overall_acc < threshold) or (inference_time > t_tar)) else 0
 
 	missed_deadline_prob = float(missed_deadline)/len(df_batches)
